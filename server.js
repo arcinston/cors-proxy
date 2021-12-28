a3561156a4b7ab2436ceb94592a20a7caf0d69c0
arcinston <arushkurundodi@gmail.com>
2021-12-28 13:25:18 +0530

    working now

--- server.js
+++ server.js
@@ -1,8 +1,37 @@
 const express = require("express");
+const axios = require("axios");
 const app = express();
-const fetch = require("node-fetch");
 const cors = require("cors");
+require("dotenv").config();
 
-app.listen(3000, () => {
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+
+app.get(":endpoint([\\/\\w\\.-]*)", (req, res) => {
+  let endpoint = process.env.API_BASE_URL + req.params.endpoint;
+  let bearerToken = process.env.API_BEARER_TOKEN;
+  console.log(endpoint);
+  console.log(bearerToken);
+  axios({
+    method: "get",
+    url: endpoint,
+    headers: {
+      Authorization: `Bearer ${bearerToken}`,
+      "Content-Type": "application/json",
+      "Access-Control-Allow-Origin": "*",
+      "X-Custom-Header": "True",
+    },
+  })
+    .then((response) => {
+      res.json(response.data);
+    })
+    .catch((error) => {
+      res.json(error);
+    });
+});
+app.listen(5000, () => {
   console.log("Server started on port 3000");
 });
